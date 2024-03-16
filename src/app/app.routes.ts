10cfd8dbffa9895eb94833f064b385b425991b0b
Yonoveleta <martinezcuestajonas@gmail.com>
2024-03-16 20:53:14 +0100

    business signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { SignupComponent } from './component/signup/signup.component';
 import { SignupEmployeeComponent } from './component/signup-employee/signup-employee.component';
+import { SignupBusinessComponent } from './component/signup-business/signup-business.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'signup-employee', component: SignupEmployeeComponent},
+    {path: 'signup-business', component: SignupBusinessComponent},
     {path: 'signup', component: SignupComponent},
     {path: '', component: LoginComponent}
 ];
---
+++ src/app/component/signup-business/signup-business.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-business',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup-business.component.html',
+  styleUrl: './signup-business.component.css'
+})
+export class SignupBusinessComponent {
+
+}
